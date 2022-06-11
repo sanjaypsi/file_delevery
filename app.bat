0377267d6151a6a23ae906293fde05413ff2fb19
Sanjay Kamble <sanjayk02@gmail.com>
2022-06-11 10:25:25 +0530

    update bat file
    Signed-off-by: Sanjay Kamble <sanjayk02@gmail.com>

--- app.bat
+++ app.bat
@@ -1,6 +1,6 @@
 set APPDIR=%~dp0
 set PYTHONEXE=%APPDIR%\python\python37\Scripts\python.exe
-set PYTHONPATH=%PYTHONPATH=%;%APPDIR%\packages;%APPDIR%\source
+set PYTHONHOME =%PYTHONHOME%;%APPDIR%\packages;%APPDIR%\source
 set SCRIPT=%APPDIR%\source\file_delivery\main.py
 
 
